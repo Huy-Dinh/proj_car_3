8183b7d9d4dd9ba73b1fbfea92d4957cf82154d6
Unknown <dinhquochuy.eeit@gmail.com>
2018-05-01 15:36:10 +0200

    Further clean up the interrupt vector table

--- src_BSW/srv/interrupt_init.c
+++ src_BSW/srv/interrupt_init.c
@@ -1,34 +1,27 @@
-/*	cint.c -- C interface for TriCore interrupts.
-
-	Copyright (C) 1998 HighTec EDV-Systeme GmbH.
-
-	This file is part of GCC.
-
-	GCC is free software; you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation; either version 3, or (at your option)
-	any later version.
-
-	GCC is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-
-	Under Section 7 of GPL version 3, you are granted additional
-	permissions described in the GCC Runtime Library Exception, version
-	3.1, as published by the Free Software Foundation.
-
-	You should have received a copy of the GNU General Public License and
-	a copy of the GCC Runtime Library Exception along with this program;
-	see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
-	<http://www.gnu.org/licenses/>.  */
+/**
+ * \file	interrupt_init.c
+ * \author	Thomas Barth 	Hochschule Darmstadt - thomas.barth@h-da.de
+ * \date 	25.04.2018
+ * \version	0.1
+ *
+ * \copyright Copyright ©2018
+ * Department of electrical engineering and information technology, Hochschule Darmstadt - University of applied sciences (h_da). All Rights Reserved.
+ * Permission to use, copy, modify, and distribute this software and its documentation for educational, and research purposes in the context of non-commercial
+ * (unless permitted by h_da) and official h_da projects, is hereby granted for enrolled students of h_da, provided that the above copyright notice,
+ * this paragraph and the following paragraph appear in all copies, modifications, and distributions.
+ * Contact Prof.Dr.-Ing. Peter Fromm, peter.fromm@h-da.de, Birkenweg 8 64295 Darmstadt - GERMANY for commercial requests.
+ *
+ * \warning This software is a PROTOTYPE version and is not designed or intended for use in production, especially not for safety-critical applications!
+ * The user represents and warrants that it will NOT use or redistribute the Software for such purposes.
+ * This prototype is for research purposes only. This software is provided "AS IS," without a warranty of any kind.
+ */
 
 #include "interrupt_init.h"
 #include "IfxCpu_reg.h"
 #include "IfxCpu_regdef.h"
 
-//#pragma section ".sys.inttab"
 
+//TODO: table for every core? [DERIVATE_NUM_CORES]
 IRQ_hdl_t Cdisptab[MAX_INTRS]={
 		{NULL,0},
 		{NULL,0},
@@ -287,9 +280,7 @@ IRQ_hdl_t Cdisptab[MAX_INTRS]={
 };
 
 
-/*==========================================
- * The common dispatcher for interrupts
- *========================================*/
+//This function is called whenever an Interrupt occurs
 void commonDispatcher()
 {
 	Ifx_CPU_ICR IcrValue = (Ifx_CPU_ICR) __MFCR(CPU_ICR);
@@ -299,63 +290,19 @@ void commonDispatcher()
 	}
 }
 
-//#pragma section
+// Interrupt Table Entry, the BIV points to this after startup
+asm (".section .interrupttable_init_in, \"ax\", @progbits");	// Create an input section
+asm("enable");													//enable interrupts
+asm("svlcx");													//save lower context
+asm("calla commonDispatcher");									//call common dispatcher function
+asm("rslcx");													//restore lower context
+asm("rfe");														//return from exception (leave ISR)
+asm (".text");													//close input section
 
-/* This is the default interrupt vector table, which consists of 32
-   entries, each consisting of eight words (32 bytes).  The table
-   must be put into the section ".inttab", must be aligned to a
-   4 KB boundary, and the entry symbol (virtual interrupt 0)
-   must be called "TriCore_int_table", as it is referenced below
-   in order to program the BIV register.  Note: This is only a
-   sample implementation which can handle up to 31 interrupts.
-   If you need more interrupts, you need to expand the table in the
-   obvious way and adjust the alignment as explained in the "TriCore
-   uC-DSP Architecture Manual".  Interrupt 1 is special in that it
-   might get used to activate services and functions of an operating
-   system.  It is therefore defined in a file called int1.o in the
-   libos.a archive, so that it can be easily redefined by a customized
-   version of __interrupt_1.  */
-
-#if defined(ERRATA_CPU13) || defined(ERRATA_DMI12)
-# if defined(ERRATA_CPU13)
-#  define DSYNC asm ("dsync");
-# else
-#  define DSYNC
-# endif
-# ifdef ERRATA_CPU9
-#   undef DSYNC
-#   define DSYNC asm ("\tdsync\n\tnop\n\tnop");
-# endif
-#else
-# define DSYNC
-#endif
-
-asm ("						\n\
-	.section .interrupttable_init_in, \"ax\", @progbits	\n\
-	.globl TriCore_int_table		\n\
-TriCore_int_table:				\n\
-");
-asm ("            \n\
-.globl ___commonDispatcher      \n\
-___commonDispatcher:          \n\
-"); 
-asm	("enable");
-asm ("svlcx");
-asm("calla commonDispatcher");
-asm("rslcx");
-asm("rfe");
-
-asm (".text");
-#if defined(ERRATA_CPU13) || defined(ERRATA_DMI12)
-  asm ("__workaround2_for_cpu13_9:");
-  asm ("ld.a %a14,[%a15+]");
-  asm ("ld.w %d4,[%a15]");
-  asm ("calli %a14");
-  asm ("rslcx");
-  asm ("rfe");
-#endif
-
-
+/* The time will come when we will need this function....
+ *
+ *
+ * but this is not today
 RC_t _install_int_hdl (uint32_t intno, void (*handler) (int), int arg){
   if(intno>=MAX_INTRS)
 	  return RC_ERROR_BAD_PARAM;
@@ -366,3 +313,4 @@ RC_t _install_int_hdl (uint32_t intno, void (*handler) (int), int arg){
   return RC_SUCCESS;
 
 }
+*/
--- src_BSW/srv/interrupt_init.h
+++ src_BSW/srv/interrupt_init.h
@@ -1,14 +1,35 @@
-/*
- * interrupt_init.h
+/**
+ * \file	interrupt_init.h
+ * \author	Thomas Barth 	Hochschule Darmstadt - thomas.barth@h-da.de
+ * \date 	25.04.2018
+ * \version	0.1
  *
- *  Created on: 18.07.2016
- *      Author: Thomas
+ * \brief TODO
+ *
+ * TODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODO
+ *
+ *	Changelog:
+ *
+ * \copyright Copyright ©2018
+ * Department of electrical engineering and information technology, Hochschule Darmstadt - University of applied sciences (h_da). All Rights Reserved.
+ * Permission to use, copy, modify, and distribute this software and its documentation for educational, and research purposes in the context of non-commercial
+ * (unless permitted by h_da) and official h_da projects, is hereby granted for enrolled students of h_da, provided that the above copyright notice,
+ * this paragraph and the following paragraph appear in all copies, modifications, and distributions.
+ * Contact Prof.Dr.-Ing. Peter Fromm, peter.fromm@h-da.de, Birkenweg 8 64295 Darmstadt - GERMANY for commercial requests.
+ *
+ * \warning This software is a PROTOTYPE version and is not designed or intended for use in production, especially not for safety-critical applications!
+ * The user represents and warrants that it will NOT use or redistribute the Software for such purposes.
+ * This prototype is for research purposes only. This software is provided "AS IS," without a warranty of any kind.
  */
+
+//TODO: doxygen docu
 
 #ifndef INTERRUPT_INIT_H_
 #define INTERRUPT_INIT_H_
 
 #include "global.h"
+
+
 
 typedef struct
 {
@@ -22,7 +43,7 @@ extern IRQ_hdl_t Cdisptab[MAX_INTRS];
 
 
 
-RC_t _install_int_hdl (uint32_t intno, void (*handler) (int), int arg);
+//RC_t _install_int_hdl (uint32_t intno, void (*handler) (int), int arg);
 
 
 
